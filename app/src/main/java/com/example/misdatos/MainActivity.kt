bbe2f3908b06c1d17f17c3a3aa103a5d36bd2763
4ubio <sebastianrubioquiroz@gmail.com>
2024-04-12 19:08:35 -0600

    Add Data Validation

--- app/src/main/java/com/example/misdatos/MainActivity.kt
+++ app/src/main/java/com/example/misdatos/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.misdatos
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,26 +18,40 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
 import com.example.misdatos.ui.theme.MisDatosTheme
+import com.example.misdatos.viewmodels.PreferencesViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val context: Context = LocalContext.current
+            val preferencesViewModel = PreferencesViewModel(context)
+
             MisDatosTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SettingsView()
+                    SettingsView(preferencesViewModel)
                 }
             }
         }
@@ -43,7 +59,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SettingsView() {
+fun SettingsView(preferencesViewModel: PreferencesViewModel) {
 
     var nombre by rememberSaveable {
         mutableStateOf("")
@@ -53,48 +69,137 @@ fun SettingsView() {
         mutableStateOf("")
     }
 
+    var altura by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    var peso by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    var hobby by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    var corrutineScope = rememberCoroutineScope()
+
+    var savedAge = preferencesViewModel.age.collectAsState(initial = 0)
+    var savedName = preferencesViewModel.name.collectAsState(initial = "")
+    var savedHeight = preferencesViewModel.height.collectAsState(initial = 0.0)
+    var savedWeight = preferencesViewModel.weight.collectAsState(initial = 0.0)
+    var savedHobby = preferencesViewModel.hobby.collectAsState(initial = "")
+
     Column (
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier = Modifier.height(10.dp))
 
-        OutlinedTextField(
-            value = nombre,
-            onValueChange = {nombre = it},
-            label = { Text("Nombre") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp)
-        )
+        if (savedName.value == "") {
+            OutlinedTextField(
+                value = nombre,
+                onValueChange = { nombre = it },
+                label = { Text("Nombre") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+            )
 
-        Spacer(modifier = Modifier.height(10.dp))
-
-        OutlinedTextField(
-            value = edad,
-            onValueChange = {edad = it},
-            label = { Text("Edad") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp)
-        )
-
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Button(
-            onClick = {},
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp)
-        ) {
-            Text(text = "Guardar datos")
+            Spacer(modifier = Modifier.height(5.dp))
         }
+
+        if (savedAge.value == 0) {
+            OutlinedTextField(
+                value = edad,
+                onValueChange = { edad = it },
+                label = { Text("Edad") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+            )
+
+            Spacer(modifier = Modifier.height(5.dp))
+        }
+
+        if (savedHeight.value == 0.0) {
+            OutlinedTextField(
+                value = altura,
+                onValueChange = { altura = it },
+                label = { Text("Altura") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+            )
+
+            Spacer(modifier = Modifier.height(5.dp))
+        }
+
+        if (savedWeight.value == 0.0) {
+            OutlinedTextField(
+                value = peso,
+                onValueChange = { peso = it },
+                label = { Text("Peso") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+            )
+
+            Spacer(modifier = Modifier.height(5.dp))
+        }
+
+        if (savedHobby.value == "") {
+            OutlinedTextField(
+                value = hobby,
+                onValueChange = {hobby = it},
+                label = { Text("Hobby") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+            )
+
+            Spacer(modifier = Modifier.height(5.dp))
+        }
+
+        if (savedName.value == "" && savedAge.value == 0 && savedHeight.value == 0.0 && savedWeight.value == 0.0 && savedHobby.value == "") {
+            Button(
+                onClick = {
+                    corrutineScope.launch {
+                        if (edad.toIntOrNull() != null && altura.toFloatOrNull() != null && peso.toFloatOrNull() != null) {
+                            preferencesViewModel.setNameAndAge(
+                                nombre,
+                                edad.toInt(),
+                                altura.toFloat(),
+                                peso.toFloat(),
+                                hobby
+                            )
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+            ) {
+                Text(text = "Guardar datos")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(text = "Nombre: ${savedName.value}", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Edad: ${savedAge.value}", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Altura: ${savedHeight.value}", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Peso: ${savedWeight.value}", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Hobby: ${savedHobby.value}", fontSize = 20.sp, fontWeight = FontWeight.Bold)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    val context: Context = LocalContext.current
+    val preferencesViewModel = PreferencesViewModel(context)
+
     MisDatosTheme {
-        SettingsView()
+        SettingsView(preferencesViewModel)
     }
 }
--- app/src/main/java/com/example/misdatos/viewmodels/PreferencesViewModel.kt
+++ app/src/main/java/com/example/misdatos/viewmodels/PreferencesViewModel.kt
@@ -1,21 +1,27 @@
 package com.example.misdatos.viewmodels
 
 import android.content.Context
+import android.view.View
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.floatPreferencesKey
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class PreferencesViewModel(val context: Context) {
+class PreferencesViewModel(val context: Context): ViewModel() {
 
     companion object {
         val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
         val NAME = stringPreferencesKey("name")
         val AGE = intPreferencesKey("age")
+        val HEIGHT = floatPreferencesKey("height")
+        val WEIGHT = floatPreferencesKey("weight")
+        val HOBBY = stringPreferencesKey("hobby")
     }
 
     //getName() asincrono
@@ -30,11 +36,32 @@ class PreferencesViewModel(val context: Context) {
                                 preferences[AGE] ?: 0
                             }
 
+    //getHeight() asincrono
+    val height: Flow<Any> = context.dataStore.data
+                            .map { preferences ->
+                                preferences[HEIGHT] ?: 0.0
+                            }
+
+    //getWeight() asincrono
+    val weight: Flow<Any> = context.dataStore.data
+                            .map { preferences ->
+                                preferences[WEIGHT] ?: 0.0
+                            }
+
+    //getHobby() asincrono
+    val hobby: Flow<String> = context.dataStore.data
+                            .map { preferences ->
+                                preferences[HOBBY] ?: ""
+                            }
+
     //setName & setAge
-    suspend fun setNameAndAge(name: String, age: Int) {
+    suspend fun setNameAndAge(name: String, age: Int, height: Float, weight: Float, hobby: String) {
         context.dataStore.edit { settings ->
             settings[NAME] = name
             settings[AGE] = age
+            settings[HEIGHT] = height
+            settings[WEIGHT] = weight
+            settings[HOBBY] = hobby
         }
     }
 }
